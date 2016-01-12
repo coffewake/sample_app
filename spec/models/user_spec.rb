c715104c5edde733a8ae2421fec44dfdf6283491
coffewake@gmail.com <coffeitunes@coffeituness-MacBook-Pro.local>
2016-01-12 04:36:08 +0400

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,7 +17,17 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin)}
   it {should be_valid}
+  it { should_not be_admin}
+
+  describe "with admin attribute 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it{should be_admin}
+  end
 
   describe "email address with mixed case" do
   
@@ -48,6 +58,7 @@ describe User do
     it { should_not eq user_for_invalid_password }
     specify { expect(user_for_invalid_password).to be_false }
   end
+
   end
 
   describe "when password is not present" do
